129734e1379233cac218c2f874ebdabbf6cc1562
Holly Deng <holly199975@gmail.com>
2018-10-02 16:31:11 +0800

    getBmiById,get data,editText

--- app/src/main/java/com/holly/bmi/MainActivity.java
+++ app/src/main/java/com/holly/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.holly.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void bmi(View view){
+        Log.d("MainActivity","bmi");
+        EditText edWeight = findViewById(R.id.ed_weight);
+        EditText edHeight = findViewById(R.id.ed_height);
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        Log.d("MainActivity",w +"/"+ h);
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("MainActivity",bmi +" ");
+    }
+
 }
